3a46f7a629c8966a0fdda91937a41b8bb2d55424
Patricia Mateica <patriciamateica33@gmail.com>
2026-04-06 18:38:03 +0300

    Added IEventUserStateService in core and EventUserStateService in UI

---
+++ movie-events/movie-events/src/MovieApp.Core/Services/IEventUserStateService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace MovieApp.Core.Services;
+
+public interface IEventUserStateService
+{
+    Task<int> GetDiscountForEventAsync(int eventId);
+    Task<bool> IsEventJoinedByUserAsync(int eventId);
+}
