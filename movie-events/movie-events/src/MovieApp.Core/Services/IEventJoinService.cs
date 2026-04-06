1721440a4dc3346eebaf6c59542e0ace60dffc18
Patricia Mateica <patriciamateica33@gmail.com>
2026-04-06 19:52:05 +0300

    created `IEventJoinService` and `EventJoinService` to handle event attendance and bonus spin logic.

---
+++ movie-events/movie-events/src/MovieApp.Core/Services/IEventJoinService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+using MovieApp.Core.Models;
+
+namespace MovieApp.Core.Services;
+
+public interface IEventJoinService
+{
+    Task<JoinEventResult> JoinEventAsync(int eventId, string buttonTag);
+}
