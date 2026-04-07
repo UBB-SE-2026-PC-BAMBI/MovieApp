23c0f013aea07297f911adc32eca1b41034605d8
Bogdan Mocrei <mocrei.bogdhan@gmail.com>
2026-04-07 21:35:02 +0300

    Styling for Ui.Converters

--- movie-events/movie-events/src/MovieApp.Ui/Converters/BoolToHighlightBrushConverter.cs
+++ movie-events/movie-events/src/MovieApp.Ui/Converters/BoolToHighlightBrushConverter.cs
@@ -1,9 +1,14 @@
+// <copyright file="BoolToHighlightBrushConverter.cs" company="MovieApp">
+// Copyright (c) MovieApp. All rights reserved.
+// </copyright>
+
+namespace MovieApp.Ui.Converters;
+
+using System;
 using Microsoft.UI;
 using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Media;
 using Windows.UI;
-
-namespace MovieApp.Ui.Converters;
 
 /// <summary>
 /// Converts a boolean to a highlight brush for jackpot events.
@@ -11,13 +16,18 @@ namespace MovieApp.Ui.Converters;
 /// </summary>
 public sealed class BoolToHighlightBrushConverter : IValueConverter
 {
+    /// <inheritdoc/>
     public object Convert(object value, Type targetType, object parameter, string language)
     {
         if (value is true)
+        {
             return new SolidColorBrush(Color.FromArgb(50, 255, 193, 7));
+        }
+
         return new SolidColorBrush(Colors.Transparent);
     }
 
+    /// <inheritdoc/>
     public object ConvertBack(object value, Type targetType, object parameter, string language)
         => throw new NotImplementedException();
 }
--- movie-events/movie-events/src/MovieApp.Ui/Converters/BoolToVisibilityConverter.cs
+++ movie-events/movie-events/src/MovieApp.Ui/Converters/BoolToVisibilityConverter.cs
@@ -1,21 +1,38 @@
-using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Data;
+// <copyright file="BoolToVisibilityConverter.cs" company="MovieApp">
+// Copyright (c) MovieApp. All rights reserved.
+// </copyright>
 
 namespace MovieApp.Ui.Converters;
 
+using System;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Data;
+
+/// <summary>
+/// Converts a boolean value to a <see cref="Visibility"/> value.
+/// Returns <see cref="Visibility.Visible"/> for true, otherwise <see cref="Visibility.Collapsed"/>.
+/// </summary>
 public sealed class BoolToVisibilityConverter : IValueConverter
 {
+    /// <inheritdoc/>
     public object Convert(object value, Type targetType, object parameter, string language)
     {
         if (value is bool boolValue)
+        {
             return boolValue ? Visibility.Visible : Visibility.Collapsed;
+        }
+
         return Visibility.Collapsed;
     }
 
+    /// <inheritdoc/>
     public object ConvertBack(object value, Type targetType, object parameter, string language)
     {
         if (value is Visibility visibility)
+        {
             return visibility == Visibility.Visible;
+        }
+
         return false;
     }
 }
